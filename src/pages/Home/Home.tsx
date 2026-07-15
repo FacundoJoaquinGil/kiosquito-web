d6021246ee18e80f43c3f39b283b09cbdc0d7f63
FacundoJoaquinGil <FacundoJoaGL@gmail.com>
2026-07-15 14:48:09 -0300

    sidebar maquetado

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,4 +1,7 @@
+import { NavLink } from "react-router-dom";
 import "./Home.css";
+import agregarProducto from "../../assets/images/agregar-producto.svg"
+import registrarVenta from "../../assets/images/registrar-venta.svg"
 
 const Home = () => {
   const nameUser: string = "Maria";
@@ -17,11 +20,45 @@ const Home = () => {
 
   return (
     <div className="home-container">
-      <h1 className="titulo-home">¡Hola, {nameUser}! 👋🏼</h1>
-      <h3 className="subtitulo-home">
-        Panel de tu kiosco <span className="kiosco-name">"{kiscoName}"</span>
-      </h3>
-      <p className="fecha">{fechaCapitalizada}</p>
+
+      <div className="user-data">
+        <h1 className="titulo-home">¡Hola, {nameUser}! 👋🏼</h1>
+        <h3 className="subtitulo-home">
+          Panel de tu kiosco <span className="kiosco-name">"{kiscoName}"</span>
+        </h3>
+        <p className="fecha">{fechaCapitalizada}</p>
+      </div>
+
+      <div className="acciones-rapidas-body">
+
+        <span className="ar-span">Acciones rápidas</span>
+
+        <ul>
+
+          <li>
+            <NavLink to="/Ventas" className="acciones-rapidas-link">
+              <img src={registrarVenta} alt="icono-registrarVenta" />
+              <div className="acciones-rapidas-cont">
+                <span>Registrar Venta</span>
+                <p className="acciones-rapidas-p">Cargá ventas del local</p>
+              </div>
+            </NavLink>
+          </li>
+
+          <li>
+            <NavLink to="/Stock" className="acciones-rapidas-link">
+              <img src={agregarProducto} alt="icono-agregarProducto" />
+              <div className="acciones-rapidas-cont">
+                <span>Agregar Producto</span>
+                <p className="acciones-rapidas-p">Sumá productos nuevos al local</p>
+              </div>
+            </NavLink>
+          </li>
+
+
+          </ul>
+
+      </div>
     </div>
   );
 };
